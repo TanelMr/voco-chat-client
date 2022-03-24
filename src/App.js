014503e76d4ea458b827851ac428c51ccadc4212
Tanel Märjama <tanel.marjama@khk.ee>
2022-03-25 01:22:42 +0200

    Added containers for message display and inputs + styling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,29 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="...">
 
+
+  return (
+<div>
+  <h2>KHK</h2>
+  <div className="container textBoxContainer">
+    <div className="messageBox">
+      <div className="messageText">
+        Lorem ipsum lorem ip  sdfadsfasdfdasfasdf
+      </div>
+      <div className="userNameandDate">
+        John Doe * 20.02.2021
+      </div>
     </div>
+  </div>
+  <div className="container inputContainer">
+    <div className="userNameInput">
+      <input type="text"/>
+    </div>
+    <div className="userTextInput">
+    </div>
+  </div>
+</div>
   );
 }
 
