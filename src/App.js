cda241e35e745f8c751486675de4e18f261a7e4f
Tanel Märjama <tanel.marjama@khk.ee>
2022-03-25 16:05:53 +0200

    Added input areas for username and message, fixed styling

--- src/App.js
+++ src/App.js
@@ -10,20 +10,31 @@ function App() {
   <div className="container textBoxContainer">
     <div className="messageBox">
       <div className="messageText">
-        Lorem ipsum lorem ip  sdfadsfasdfdasfasdf
+        Lorem ipsum lorem ip
       </div>
-      <div className="userNameandDate">
+      <div className="userNameAndDate">
         John Doe * 20.02.2021
       </div>
     </div>
   </div>
-  <div className="container inputContainer">
-    <div className="userNameInput">
-      <input type="text"/>
+
+  <div className="container-fluid inputContainer">
+    <div className="row">
+      <div className="col-3 ">
+        <textarea placeholder="Username" maxLength="20"/>
+      </div>
+      <div className="col-9">
+        <textarea placeholder="Type your message..." maxLength="200"/>
+      </div>
     </div>
-    <div className="userTextInput">
+    <div className="row">
+      <div className="col">
+        <button type="button" id="button" className="button btn btn-dark">Send</button>
+      </div>
     </div>
   </div>
+
+
 </div>
   );
 }
